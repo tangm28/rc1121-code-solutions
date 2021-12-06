adf6dca5e3c97658d7c49f122c91688dbae037eb
Michael Tang <tngmichael28@gmail.com>
2021-12-06 11:55:16 -0800

    Completed javascript operators and expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 25;
+var height = 50;
+var area = width * height;
+
+console.log('value of area: ' + area);
+console.log('type of area: ' + typeof area);
+
+var bill = 100;
+var payment = 120;
+var change = payment - bill;
+
+console.log('value of change: ' + change);
+console.log('type of change: ' + typeof change);
+
+var quizzes = 80;
+var midterm = 72;
+var final = 76;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade: ' + grade);
+console.log('type of grade: ' + typeof grade);
+
+var firstName = 'Leeroy';
+var lastName = 'Jenkins';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName: ' + fullName);
+console.log('type of fullName: ' + typeof fullName);
+
+var headCount = 7;
+var isSparta = headCount === 3;
+
+console.log('value of isSparta: ' + isSparta);
+console.log('type of isSparta: ' + typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+
+console.log('value of motto: ' + motto);
+console.log('type of motto: ' + typeof motto);

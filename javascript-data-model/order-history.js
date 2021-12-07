6e485bfa140a7d2c24f9f0f0aee398cc79919192
Michael Tang <tngmichael28@gmail.com>
2021-12-07 11:25:07 -0800

    Completed javascript data model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,75 @@
+var orderHistory = [
+  {
+    orderNum: '113-2883177-2648248',
+    shipTo: 'JS Masher',
+    orderDate: ['July', 3, 2020],
+    deliveredDate: ['Jul', 5, 2020],
+    delivered: true,
+    cart: [
+      {
+        itemLink: 'GameCube Controller',
+        isBook: false,
+        bookAuthor: '',
+        returnWindow: ['Jul', 5, 2020],
+        cost: 94.95
+      },
+
+      {
+        itemLink: 'The Art of Sql',
+        isBook: true,
+        bookAuthor: 'Faroult, Stephane',
+        returnWindow: ['Jul', 5, 2020],
+        cost: 33.99
+      }],
+    total: 138.93
+  },
+
+  {
+    orderNum: '114-2875557-9059409',
+    shipTo: 'JS Masher',
+    orderDate: ['July', 4, 2020],
+    deliveredDate: ['Jul', 7, 2020],
+    delivered: true,
+    cart: [{
+      itemLink: 'Gamecub Controller Adapter',
+      isBook: false,
+      bookAuthor: '',
+      returnWindow: ['Aug', 5, 2020],
+      cost: 15.98
+    }],
+    total: 17.22
+  },
+
+  {
+    orderNum: '113-9984268-128057',
+    shipTo: 'JS Masher',
+    orderDate: ['July', 19, 2020],
+    deliveredDate: ['Jul', 19, 2020],
+    delivered: true,
+    cart: [{
+      itemLink: 'The Timeless Way of Building',
+      isBook: true,
+      bookAuthor: 'Alexander, Christopher',
+      returnWindow: ['Aug', 19, 2020],
+      cost: 41.33
+    }],
+    total: 44.53
+  },
+
+  {
+    orderNum: '114-3941689-8772232',
+    shipTo: 'JS Masher',
+    orderDate: ['August', 4, 2020],
+    deliveredDate: ['Aug', 8, 2020],
+    delivered: true,
+    cart: [{
+      itemLink: 'Javascript for impatient programmers',
+      isBook: true,
+      bookAuthor: 'Rauschmayer, Dr. Axel',
+      returnWindow: ['Sept', 7, 2020],
+      cost: 31.55
+    }],
+    total: 34
+  }
+
+];

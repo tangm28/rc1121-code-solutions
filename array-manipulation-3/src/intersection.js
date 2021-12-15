db673486b77cf11347ae05a818e0493032892687
Michael Tang <tngmichael28@gmail.com>
2021-12-15 13:40:43 -0800

    Finished array manipulation 3 challenge.

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,28 @@
 /* exported intersection */
+function intersection(first, second) {
+  var length = first.length + second.length;
+  var newArray = [];
+  var tempArray = [];
+  for (var i = 0; i < length; i++) {
+    if (i >= first.length) {
+      tempArray.push(second[i - first.length]);
+    } else {
+      tempArray.push(first[i]);
+    }
+  }
+  for (var j = 0; j < tempArray.length; j++) {
+    var counter = 0;
+    var tempHolder = tempArray[j];
+    for (var k = 0; k < tempArray.length; k++) {
+      if (tempArray[k] === tempHolder) {
+        counter++;
+      }
+    }
+    if (counter > 1) {
+      if (newArray.includes(tempHolder) === false) {
+        newArray.push(tempHolder);
+      }
+    }
+  }
+  return newArray;
+}
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,19 @@
 /* exported union */
+function union(first, second) {
+  var length = first.length + second.length;
+  var newArray = [];
+  var tempArray = [];
+  for (var i = 0; i < length; i++) {
+    if (i >= first.length) {
+      if (newArray.includes(second[i - first.length]) === false) {
+        newArray.push(second[i - first.length]);
+      }
+      tempArray.push(second[i - first.length]);
+    } else {
+      if (newArray.includes(first[i]) === false) {
+        newArray.push(first[i]);
+      }
+    }
+  }
+  return newArray;
+}

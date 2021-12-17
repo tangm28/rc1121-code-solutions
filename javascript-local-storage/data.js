218aa00dd112950327c309c4c9dcbab2f6741237
Michael Tang <tngmichael28@gmail.com>
2021-12-17 12:45:49 -0800

    fixed the missing parse.

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -3,10 +3,9 @@
 var todos = [];
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
 if (previousTodosJSON !== null) {
-  todos = [];
+  todos = JSON.parse(previousTodosJSON);
 }
 window.addEventListener('beforeunload', function (event) {
-  event.preventDefault();
   var todosJSON = JSON.stringify(todos);
   this.localStorage.setItem('javascript-local-storage', todosJSON);
 });

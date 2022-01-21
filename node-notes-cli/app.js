e2af818e613ec4223cf6faf8d4acc545d206dc1c
Michael Tang <tngmichael28@gmail.com>
2022-01-20 16:01:57 -0800

    Removed the extra writefile.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -5,27 +5,24 @@ const action = process.argv[2];
 const input = process.argv[3];
 const update = process.argv[4];
 
-fs.writeFile(file, JSON.stringify(data, null, 2), err => {
-  if (err) throw err;
-  if (action === 'read') {
-    for (const keys in data.notes) {
-      console.log(keys + ': ' + data.notes[keys]);
-    }
-  } else if (action === 'add') {
-    data.notes[data.nextId] = input;
-    data.nextId++;
-    fs.writeFile(file, JSON.stringify(data, null, 2), err => {
-      if (err) throw err;
-    });
-  } else if (action === 'delete') {
-    delete data.notes[input];
-    fs.writeFile(file, JSON.stringify(data, null, 2), err => {
-      if (err) throw err;
-    });
-  } else if (action === 'update') {
-    data.notes[input] = update;
-    fs.writeFile(file, JSON.stringify(data, null, 2), err => {
-      if (err) throw err;
-    });
+if (action === 'read') {
+  for (const keys in data.notes) {
+    console.log(keys + ': ' + data.notes[keys]);
   }
-});
+} else if (action === 'add') {
+  data.notes[data.nextId] = input;
+  data.nextId++;
+  fs.writeFile(file, JSON.stringify(data, null, 2), err => {
+    if (err) throw err;
+  });
+} else if (action === 'delete') {
+  delete data.notes[input];
+  fs.writeFile(file, JSON.stringify(data, null, 2), err => {
+    if (err) throw err;
+  });
+} else if (action === 'update') {
+  data.notes[input] = update;
+  fs.writeFile(file, JSON.stringify(data, null, 2), err => {
+    if (err) throw err;
+  });
+}

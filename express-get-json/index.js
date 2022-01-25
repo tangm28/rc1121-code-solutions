6babb93da0ab0db7b6067c168cc90cc26af531d3
Michael Tang <tngmichael28@gmail.com>
2022-01-25 12:03:56 -0800

    Completed express get json exercise.

---
+++ express-get-json/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.use((req, res, next) => {
+  const data = [];
+  for (const key in grades) {
+    data.push(grades[key]);
+  }
+  res.json(data);
+  next();
+});
+
+app.get('/api/grades', (req, res) => {
+  res.send();
+});
+
+app.listen(3000);
